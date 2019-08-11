a6f4185c7925fe3e778148d5c45c4ebe2177d774
Adriano <adriano.selaviles@gmail.com>
2019-08-10 21:28:17 -0700

    tests

--- multikey_test.go
+++ multikey_test.go
@@ -8,9 +8,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// We test the following statement:
+// For a secret encrypted with n required keys {n:n ∈ ℕ}, n keys are
+// necessary and sufficient to decrypt the secret
 func TestEncryptDecrypt(t *testing.T) {
+	n := 10
+	testSecret := []byte("test secret value")
 	privs, pubs := []*rsa.PrivateKey{}, []*rsa.PublicKey{}
-	for i := 0; i < 10; i++ {
+
+	// generate n keys
+	for k := 0; k < n; k++ {
 		pri, pub, err := keys.GenerateRSAKeyPair(2048)
 		if err != nil {
 			assert.Fail(t, "could not generate test keys")
@@ -18,14 +25,23 @@ func TestEncryptDecrypt(t *testing.T) {
 		privs = append(privs, pri)
 		pubs = append(pubs, pub)
 	}
-	testSecret := []byte("test secret value")
-	s, err := Encrypt(testSecret, pubs, 1)
-	if err != nil {
-		assert.Fail(t, "could not encrypt test secret")
+
+	// encrypt with 1 to n keys
+	for e := 1; e <= n; e++ {
+		s, err := Encrypt(testSecret, pubs, e)
+		if err != nil {
+			assert.Fail(t, "could not encrypt test secret")
+		}
+		// decrypt unsuccessfully with 1 to e keys
+		for d := 1; d < e; d++ {
+			plain, _ := Decrypt(s, privs[:d])
+			assert.NotEqual(t, plain, testSecret)
+		}
+		// decrypt successfully with e to n keys
+		for d := e; d <= n; d++ {
+			plain, err := Decrypt(s, privs[:d])
+			assert.Nil(t, err)
+			assert.Equal(t, plain, testSecret)
+		}
 	}
-	plain, err := Decrypt(s, privs)
-	if err != nil {
-		assert.Fail(t, "could not decrypt test secret")
-	}
-	assert.Equal(t, plain, testSecret)
 }
--- secret_test.go
+++ secret_test.go
@@ -1,3 +1,76 @@
 package multikey
 
-// TODO
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncodePEM(t *testing.T) {
+	// TODO
+}
+
+func TestDecodePEM(t *testing.T) {
+	// TODO
+}
+
+func TestEncodeSimple(t *testing.T) {
+	// TODO
+}
+
+func TestDecodeSimple(t *testing.T) {
+	// TODO
+}
+
+func TestEncodeDecodePEM(t *testing.T) {
+	tests := []struct {
+		testName      string
+		testSecret    *Secret
+		expectErr     bool
+		expectedError string
+	}{
+		{
+			testName: "positive test",
+			testSecret: &Secret{
+				shards: []*encryptedShard{
+					{
+						KeyID: "some key id",
+						Value: "asdfghjkl",
+					},
+					{
+						KeyID: "some key id",
+						Value: "asdfghjkl",
+					},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			testName: "positive test helper",
+			testSecret: &Secret{
+				shards: []*encryptedShard{
+					{
+						KeyID:  "some key id",
+						Value:  "asdfghjkl",
+						Helper: "asdfghjkl",
+					},
+				},
+			},
+			expectErr: false,
+		},
+	}
+
+	for _, test := range tests {
+		enc, err := test.testSecret.EncodePEM()
+		if test.expectErr {
+			assert.EqualError(t, err, test.expectedError, test.testName)
+			continue
+		}
+		dec, err := DecodePEM(enc)
+		if test.expectErr {
+			assert.EqualError(t, err, test.expectedError, test.testName)
+			continue
+		}
+		assert.EqualValues(t, dec, test.testSecret, test.testName)
+	}
+}
